079a6f635de658a8cb47aafeca92ec084e5ae0cb
satyaprakash-yadav <satyay3556@gmail.com>
2023-07-11 21:27:06 +0530

    first commit

---
+++ jQuery.js
@@ -0,0 +1,67 @@
+                                                        // JQUERY INTRODUCTION
+
+// Important Question 
+
+// What is jQuery ?
+// 1. jQuery is a JavaScript Library.
+// 2. Write less do more.
+// 3. jQuery greatly simplifies JavaScript programming
+// 4. The purpose of jQuery is to make it much easier to use JavaScript on your website
+// 5. jQuery takes a lot of common tasks that require many lines of JavaScript code to accomplish , and wraps them into methods
+//    that you can with a single line of code.
+
+// Purpose of jQuery
+// a. Simplify HTML DOM tree traversal and manipulation , as well as event handling, CSS Animation, and Ajax
+// b. jQuery will run exactly the same in all major browser.(browser compatibility)
+
+
+// 2. Should you learn jQuery ?
+// Modern JavaScript Solved all the problem that jQuery was solving. Then Why learn jQuery?
+
+// Answer : jQuery has plugins for almost any task out there.
+
+
+// JQuery perform to operations
+// 1. select html element
+// 2. perform jquery function/or action on selected element.
+
+// add jquery cdn link in html page before <body> tag.
+
+// syntax :- 
+
+// $("#para") ---> Identity of jquery
+//   selector
+//      |
+// select element by id 
+
+// how to perform action on particular id then jquery has own "function" u have to use it.
+
+// $(selector).action();   // jQuery syntax 
+
+// $("#para").hide();      // JQuery Example
+
+
+// jQuery Selectors :-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
